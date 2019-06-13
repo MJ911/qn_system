1e6aef6e590955d60d4395439165254144076813
majiajie <majiajie@qq.com>
2019-06-13 16:49:54 +0800

    修改了AdministratorController包引入问题

--- qn_system/src/main/java/com/nwafu/qn_system/web/AdministratorController.java
+++ qn_system/src/main/java/com/nwafu/qn_system/web/AdministratorController.java
@@ -6,12 +6,12 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.nwafu.qn_system.dao.AuthorityDAO;
 import com.nwafu.qn_system.dao.QuestionnaireDAO;
