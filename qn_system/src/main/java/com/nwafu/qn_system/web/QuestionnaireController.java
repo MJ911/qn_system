ffd78d06c07fe536cbc28bb2e54ddcca15de79e0
majiajie <majiajie@qq.com>
2019-06-17 15:26:20 +0800

    questionnaire_list的分页操作完成

--- qn_system/src/main/java/com/nwafu/qn_system/web/QuestionnaireController.java
+++ qn_system/src/main/java/com/nwafu/qn_system/web/QuestionnaireController.java
@@ -2,12 +2,19 @@ package com.nwafu.qn_system.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.nwafu.qn_system.dao.QuestionnaireDAO;
 import com.nwafu.qn_system.entity.Answer;
 import com.nwafu.qn_system.entity.Question;
 import com.nwafu.qn_system.entity.Questionnaire;
@@ -22,6 +29,9 @@ public class QuestionnaireController {
      */
 	@Autowired
 	private QnService qnService;
+	
+	@Autowired
+	private QuestionnaireDAO questionnaireDAO;
 	
 	@PostMapping("questionnaire_list")
 	public String questionnaire(Questionnaire questionnaire) {
@@ -50,6 +60,136 @@ public class QuestionnaireController {
 		}
 		System.out.println(questionnaire); 
 		qnService.answerQn(questionnaire);
+		return "questionnaire_list";
+	}
+	
+	//分页操作下一页（questionnaire_list）
+	@GetMapping("NextPage")
+	public String NextPage(Model model,HttpSession session) {
+		/*
+		 * 返回问卷列表 questionnaire_list.jsp
+		 * 往model里面加调查或者投票的List
+		 * questionnaire_type为0视为调查
+		 * questionnaire_type为1视为投票
+		 * questionnaire_type为2为管理问卷列表
+		 */
+		//分页查询设置
+		int indexPage = (int)session.getAttribute("indexPage");
+		indexPage++;
+		PageHelper.startPage(indexPage,10);// 设置分页，参数1=页数，参数2=每页显示条数
+		List<Questionnaire> questionnairelist = null;
+		int questionnaire_type = (int)session.getAttribute("questionnaire_type");
+		questionnairelist = questionnaireDAO.getAllByquestionnaire_type(questionnaire_type);
+		PageInfo<Questionnaire> pageInfo = new PageInfo<Questionnaire>(questionnairelist);
+		
+		model.addAttribute("questionnairelist", questionnairelist);
+		session.setAttribute("lines",pageInfo.getTotal());//总记录数
+		session.setAttribute("pages",pageInfo.getPages());//总页数
+		session.setAttribute("indexPage",pageInfo.getPageNum());//当前界面
+		return "questionnaire_list";
+	}
+	
+	//分页操作上一页（questionnaire_list）
+	@GetMapping("PrePage")
+	public String PrePage(Model model,HttpSession session) {
+		/*
+		 * 返回问卷列表 questionnaire_list.jsp
+		 * 往model里面加调查或者投票的List
+		 * questionnaire_type为0视为调查
+		 * questionnaire_type为1视为投票
+		 * questionnaire_type为2为管理问卷列表
+		 */
+		//分页查询设置
+		int indexPage = (int)session.getAttribute("indexPage");
+		indexPage--;
+		PageHelper.startPage(indexPage,10);// 设置分页，参数1=页数，参数2=每页显示条数
+		List<Questionnaire> questionnairelist = null;
+		int questionnaire_type = (int)session.getAttribute("questionnaire_type");
+		questionnairelist = questionnaireDAO.getAllByquestionnaire_type(questionnaire_type);
+		PageInfo<Questionnaire> pageInfo = new PageInfo<Questionnaire>(questionnairelist);
+		
+		model.addAttribute("questionnairelist", questionnairelist);
+		session.setAttribute("lines",pageInfo.getTotal());//总记录数
+		session.setAttribute("pages",pageInfo.getPages());//总页数
+		session.setAttribute("indexPage",pageInfo.getPageNum());//当前界面
+		return "questionnaire_list";
+	}
+	
+	//分页操作首页（questionnaire_list）
+	@GetMapping("ToPageFirst")
+	public String ToPageFirst(Model model,HttpSession session) {
+		/*
+		 * 返回问卷列表 questionnaire_list.jsp
+		 * 往model里面加调查或者投票的List
+		 * questionnaire_type为0视为调查
+		 * questionnaire_type为1视为投票
+		 * questionnaire_type为2为管理问卷列表
+		 */
+		//分页查询设置
+		int indexPage = (int)session.getAttribute("indexPage");
+		indexPage=1;
+		PageHelper.startPage(indexPage,10);// 设置分页，参数1=页数，参数2=每页显示条数
+		List<Questionnaire> questionnairelist = null;
+		int questionnaire_type = (int)session.getAttribute("questionnaire_type");
+		questionnairelist = questionnaireDAO.getAllByquestionnaire_type(questionnaire_type);
+		PageInfo<Questionnaire> pageInfo = new PageInfo<Questionnaire>(questionnairelist);
+		
+		model.addAttribute("questionnairelist", questionnairelist);
+		session.setAttribute("lines",pageInfo.getTotal());//总记录数
+		session.setAttribute("pages",pageInfo.getPages());//总页数
+		session.setAttribute("indexPage",pageInfo.getPageNum());//当前界面
+		return "questionnaire_list";
+	}
+	
+	//分页操作尾页（questionnaire_list）
+	@GetMapping("ToPageEnd")
+	public String ToPageEnd(Model model,HttpSession session) {
+		/*
+		 * 返回问卷列表 questionnaire_list.jsp
+		 * 往model里面加调查或者投票的List
+		 * questionnaire_type为0视为调查
+		 * questionnaire_type为1视为投票
+		 * questionnaire_type为2为管理问卷列表
+		 */
+		//分页查询设置
+		int indexPage = (int)session.getAttribute("indexPage");
+		indexPage=(int)session.getAttribute("pages");
+		PageHelper.startPage(indexPage,10);// 设置分页，参数1=页数，参数2=每页显示条数
+		List<Questionnaire> questionnairelist = null;
+		int questionnaire_type = (int)session.getAttribute("questionnaire_type");
+		questionnairelist = questionnaireDAO.getAllByquestionnaire_type(questionnaire_type);
+		PageInfo<Questionnaire> pageInfo = new PageInfo<Questionnaire>(questionnairelist);
+		
+		model.addAttribute("questionnairelist", questionnairelist);
+		session.setAttribute("lines",pageInfo.getTotal());//总记录数
+		session.setAttribute("pages",pageInfo.getPages());//总页数
+		session.setAttribute("indexPage",pageInfo.getPageNum());//当前界面
+		return "questionnaire_list";
+	}
+	
+	//分页操作跳转页（questionnaire_list）
+	@GetMapping("/ToPageIndex/{index}")
+	public String ToPageIndex(@PathVariable String index,Model model,HttpSession session) {
+		/*
+		 * 返回问卷列表 questionnaire_list.jsp
+		 * 往model里面加调查或者投票的List
+		 * questionnaire_type为0视为调查
+		 * questionnaire_type为1视为投票
+		 * questionnaire_type为2为管理问卷列表
+		 */
+		//分页查询设置
+		int indexPage = (int)session.getAttribute("indexPage");
+		indexPage=new Integer(index);
+		PageHelper.startPage(indexPage,10);// 设置分页，参数1=页数，参数2=每页显示条数
+		List<Questionnaire> questionnairelist = null;
+		int questionnaire_type = (int)session.getAttribute("questionnaire_type");
+		questionnairelist = questionnaireDAO.getAllByquestionnaire_type(questionnaire_type);
+		PageInfo<Questionnaire> pageInfo = new PageInfo<Questionnaire>(questionnairelist);
+		
+		model.addAttribute("questionnairelist", questionnairelist);
+		session.setAttribute("lines",pageInfo.getTotal());//总记录数
+		session.setAttribute("pages",pageInfo.getPages());//总页数
+		session.setAttribute("indexPage",pageInfo.getPageNum());//当前界面
 		return "questionnaire_list";
 	}
 }
